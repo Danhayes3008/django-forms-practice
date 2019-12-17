1f092724c7a23215b3b87e090464fd3e5c992308
danhayes <danhayes3008@outlook.com>
2019-12-17 19:10:58 +0000

    updating the repo

--- forms/admin.py
+++ forms/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import userDetails
+from .models import Profile
 
 # Register your models here.
-admin.site.register(userDetails)
+admin.site.register(Profile)
--- forms/views.py
+++ forms/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import userDetails
+from .models import Profile
 
 # Create your views here.
 def home(request):
