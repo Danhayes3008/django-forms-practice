bd89c2f29b0f14375d0f19e1062b04bd3d81f985
danhayes <danhayes3008@outlook.com>
2019-12-18 11:58:28 +0000

    saving the changes to this so it is all up to date

--- forms/views.py
+++ forms/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+# from django.contrib.auth.models import User
 from .models import Profile
 
 # Create your views here.
 def home(request):
+    # my_user = User.objects.get(username='whatever')
+    # profile = my_user.profile
     return render(request, "index.html")
